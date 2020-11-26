542fc9a488bb87f241f61c99b9b9020a74c5203a
Noel Thomson <noel.thomson@gmail.com>
2020-11-25 22:12:59 -0500

    second commit

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -22,12 +22,12 @@ window.onscroll = () => {
   if (window.scrollY === 0) {
     document.getElementById('navbar').classList.remove('white');
   } else if (prevScrollPos > currentScrollPos) {
-    // if (!navbar.classList.contains('open')) {
     document.getElementById('navbar').classList.remove('hide');
     document.getElementById('navbar').classList.add('white');
-    // }
   } else {
-    document.getElementById('navbar').classList.add('hide');
+    if (!navbar.classList.contains('open')) {
+      document.getElementById('navbar').classList.add('hide');
+    }
   }
   prevScrollPos = currentScrollPos;
 };
